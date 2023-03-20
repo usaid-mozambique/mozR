a33079bdc34d3e6c25adcff7c33d746cf5c6d090
josephlara <joseph.lara@gmail.com>
2023-03-20 23:15:43 +0200

    mozR development: * add ats results indicator mapping data

--- R/data.R
+++ R/data.R
@@ -1,10 +1,10 @@
-#' Name of cascade plots available to the user
+#' SISMA CPN indicator Map for Feature Engineering
 #'
 #' List of indicators
 #'
 #' @usage data(data_sisma_smi_cpn_map)
 #'
-#' @format A list of the plots available to the user
+#' @format A list of the variables included to engineer dataset features
 #' \describe{
 #'   \item{data_sisma_smi_cpn_map}{indicator}
 #'   \item{data_sisma_smi_cpn_map}{indicator_new}
@@ -13,3 +13,25 @@
 #' }
 
 "data_sisma_smi_cpn_map"
+
+
+
+#' SISMA ATS Results indicator Map for Feature Engineering
+#'
+#' List of indicators
+#'
+#' @usage data(data_sisma_ats_results)
+#'
+#' @format A list of the variables included to engineer dataset features
+#' \describe{
+#'   \item{data_sisma_ats_results}{indicator}
+#'   \item{data_sisma_ats_results}{indicator_new}
+#'   \item{data_sisma_ats_results}{sex}
+#'   \item{data_sisma_ats_results}{age}
+#'   \item{data_sisma_ats_results}{age_coarse}
+#'   \item{data_sisma_ats_results}{modality}
+#'   \item{data_sisma_ats_results}{result_status}
+#'   \item{data_sisma_ats_results}{disaggregate}
+#' }
+
+"data_sisma_ats_results"
