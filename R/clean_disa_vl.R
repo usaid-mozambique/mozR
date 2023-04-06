b48e1ed009ebe2194bb9b7e08033e121232cdac0
josephlara <joseph.lara@gmail.com>
2023-04-06 12:14:37 +0200

    mozR dev: * clean_disa_vl update to pull psnuuid and manually define for _Military

--- R/clean_disa_vl.R
+++ R/clean_disa_vl.R
@@ -6,52 +6,59 @@
 #' @export
 #'
 #' @examples
+#' \dontrun{
 #'
 #'  df <- clean_disa_vl()}
 
 clean_disa_vl <- function(df){
 
   df <- df %>%
-    drop_na(datim_uid) %>%
-    select(!c(snu, psnu, sitename)) %>%
+    tidyr::drop_na(datim_uid) %>%
+    dplyr::select(!c(snu, psnu, sitename)) %>%
 
-    left_join(datim_orgsuids, by = c("datim_uid" = "datim_uid")) %>%
-    left_join(ajuda_site_map, by = c("datim_uid" = "datim_uid")) %>%
+    dplyr::left_join(datim_orgsuids, by = "datim_uid") %>%
+    dplyr::left_join(ajuda_site_map, by = "datim_uid") %>%
+    dplyr::left_join(psnuuid_map, by = "psnu") %>%
 
-    mutate(
-      partner = replace_na(partner, "MISAU"),
-      support_type = case_when(
+    dplyr::mutate(
+      partner = tidyr::replace_na(partner, "MISAU"),
+      support_type = dplyr::case_when(
         partner == "MISAU" ~ "Sustainability",
         TRUE ~ as.character("AJUDA")),
 
-      agency = case_when(
+      agency = dplyr::case_when(
         partner == "MISAU" ~ "MISAU",
         partner == "JHPIEGO-DoD" ~ "DOD",
         partner == "ECHO" ~ "USAID",
         TRUE ~ as.character("HHS/CDC")),
 
-      across(c(snu, psnu, sitename), ~ case_when(partner == "JHPIEGO-DoD" ~ "_Military",
-                                                 TRUE ~ .))) %>%
+      psnuuid = dplyr::case_when(
+        partner == "JHPIEGO-DoD" ~ "siMZUtd2cJW",
+        TRUE ~ psnuuid),
 
-    select(period,
-           sisma_uid,
-           datim_uid,
-           site_nid,
-           starts_with("his_"),
-           snu,
-           psnu,
-           sitename,
-           support_type,
-           partner,
-           agency,
-           age,
-           group,
-           sex,
-           motive,
-           tat_step,
-           VL,
-           VLS,
-           TAT)
+      dplyr::across(c(snu, psnu, sitename), ~ dplyr::case_when(partner == "JHPIEGO-DoD" ~ "_Military",
+                                                               TRUE ~ .))) %>%
+
+    dplyr::select(period,
+                  sisma_uid,
+                  datim_uid,
+                  site_nid,
+                  starts_with("his_"),
+                  snu,
+                  psnu,
+                  psnuuid,
+                  sitename,
+                  support_type,
+                  partner,
+                  agency,
+                  age,
+                  group,
+                  sex,
+                  motive,
+                  tat_step,
+                  VL,
+                  VLS,
+                  TAT)
 
   return(df)
 
