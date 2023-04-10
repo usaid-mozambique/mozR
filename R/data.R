6c9b90d6cd1335a4840feb7d07af73ae2fbf8ee0
josephlara <joseph.lara@gmail.com>
2023-04-10 15:34:03 +0200

    mozR dev: * add em_var_mapping data * add extract_em_meta function

--- R/data.R
+++ R/data.R
@@ -177,3 +177,17 @@
 #'   \item{data_sisma_smi_mat_map}{cause_type}
 #' }
 "data_sisma_smi_mat_map"
+
+
+#' Map for renaming variables contained in Enhanced Monitoring partner submission
+#'
+#' List of indicators
+#'
+#' @usage data(data_em_imer_var_map)
+#'
+#' @format A mapping of variable names and recoded variable names
+#' \describe{
+#'   \item{data_em_imer_var_map}{indicator}
+#'   \item{data_em_imer_var_map}{indicator_new}
+#' }
+"data_em_imer_var_map"
