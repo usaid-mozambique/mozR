fef2197d3172f0ac708f9b5da170d8eed23715a1
Lara <jlara@usaid.gov>
2023-10-02 11:37:18 +0200

    mozR dev: * addition of reshape_em_ahd

---
+++ R/reshape_em_ahd.R
@@ -0,0 +1,42 @@
+#' Process monthly enhanced monitoring TB AHD submission from PEPFAR Mozambique Clinical Partners
+#'
+#' @param filename Local path to the monthly IP submission
+#'
+#' @return A tidy dataframe with monthly TB AHD results.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' df <- reshape_em_ahd()}
+
+reshape_em_ahd <- function(filename){
+  ip_temp <- mozR::extract_em_meta(filename, type = "ip")
+  month_temp <- mozR::extract_em_meta(filename, type = "month")
+
+  df <- readxl::read_excel(filename,
+                           sheet = "AHD TB",
+                           skip = 4,
+                           .name_repair = "unique_quiet") %>%
+
+    dplyr::select(!c(contains("remove")),
+                  sisma_nid) %>%
+    tidyr::pivot_longer('CD4.Elig__Male_u1':'TB.TX_GradeNR',
+                        names_to = c("indicator", "disaggregate", "sex", "age"),
+                        names_sep = "_",
+                        values_to = "value") %>%
+    dplyr::mutate(period = month_temp,
+                  partner = ip_temp,
+                  indicator = stringr::str_replace_all(indicator, "\\.", "_"),
+                  age = dplyr::recode(age,
+                                      "Unk" = "Unknown Age",
+                                      "u1" = "<01",
+                                      "1.4" = "01-04",
+                                      "5.9" = "05-09",
+                                      "10.14" = "10-14",
+                                      "15.19" = "15-19",
+                                      "20" = "20+"),# new code to correct age
+    )
+
+  return(df)
+}
